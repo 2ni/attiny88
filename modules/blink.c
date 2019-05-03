bf4d666bdd2b621d0dc1bfb158830b25e18b3057
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2019-05-03 20:18:26 +0200

    moved general functions and definitions to its own file

--- modules/blink.c
+++ modules/blink.c
@@ -8,65 +8,12 @@
 #include <util/delay.h>
 //#include "avr_print.h"
 #include "uart.h"
-
-#define DDR_G DDRA
-#define PORT_G PORTA
-#define LED_G PA3
-
-#define DDR_R DDRB
-#define PORT_R PORTB
-#define LED_R PB6
-
-#define DDR_B DDRB
-#define PORT_B PORTB
-#define LED_B PB7
-
+#include "def.h"
 
 // _BV(3) => 1 << 3 => 0x08
 // PORTB = PORTB | (1 << 4);
 // set: PORTB |= (1<<BIT2); PORTB |= _BV(1) | BV(2);
 // clear: PORTB &= ~(1<<BIT2); PORTB &= _BV(1) & _BV(2);
-
-inline static void ledSetup(){
-  DDR_G |= _BV(LED_G); // enable as output (set 1)
-  PORT_G &= ~_BV(LED_G); // set low
-
-  DDR_B |= _BV(LED_B); // enable as output (set 1)
-  PORT_B &= ~_BV(LED_B); // set low
-
-  DDR_R |= _BV(LED_R); // enable as output (set 1)
-  PORT_R &= ~_BV(LED_R); // set low
-}
-
-inline static void ledOn(char color) {
-  if (color == 'g') {
-    PORT_G |= _BV(LED_G); // set high
-  } else if (color == 'b') {
-    PORT_B |= _BV(LED_B);
-  } else if (color == 'r') {
-    PORT_R |= _BV(LED_R);
-  }
-}
-
-inline static void ledOff(char color) {
-  if (color == 'g') {
-    PORT_G &= ~_BV(LED_G); // set low
-  } else if (color == 'b') {
-    PORT_B &= ~_BV(LED_B);
-  } else if (color == 'r') {
-    PORT_R &= ~_BV(LED_R);
-  }
-}
-
-inline static void ledToggle(char color) {
-  if (color == 'g') {
-    PORT_G ^= _BV(LED_G); // invert (exclusive or)
-  } else if (color == 'b') {
-    PORT_B ^= _BV(LED_B);
-  } else if (color == 'r') {
-    PORT_R ^= _BV(LED_R);
-  }
-}
 
 int main(void) {
   /*
---
+++ modules/common/def.h
@@ -0,0 +1,74 @@
+#define DDR_G DDRA
+#define PORT_G PORTA
+#define LED_G PA3
+
+#define DDR_R DDRB
+#define PORT_R PORTB
+#define LED_R PB6
+
+#define DDR_B DDRB
+#define PORT_B PORTB
+#define LED_B PB7
+
+#define DEBUG PD3
+#define TOUCH1 PD5
+#define TOUCH2 PD6
+#define TOUCH3 PD7
+
+#define TEMP PA0      // 0°=0.81v, 10°=0.55v, 25°=0.3v, 50°=0.11v
+#define BAT PA1       // battery (0.76v = 4.2v, 0.5v = 2.8v)
+#define PWM PB1       // buzzer
+#define CLKO PB0      // clock out
+#define EN_LIGHT PC1
+#define LIGHT PC0     // dark: 1.03v, light: 0.03v
+
+#define MOIST_A PD4   // 1touch, qtouch
+#define MOIST_B PA2   // qtouch
+
+#define EN_CUR PC7    // enable current measurement
+#define CUR PC2       // 2.15mA/bit, 2.2A=1.1v
+#define OUT PC3
+
+#define SDA PC4
+#define SCL PC5
+
+inline static void ledSetup() {
+  DDR_G |= _BV(LED_G); // enable as output (set 1)
+  PORT_G &= ~_BV(LED_G); // set low
+
+  DDR_B |= _BV(LED_B); // enable as output (set 1)
+  PORT_B &= ~_BV(LED_B); // set low
+
+  DDR_R |= _BV(LED_R); // enable as output (set 1)
+  PORT_R &= ~_BV(LED_R); // set low
+}
+
+inline static void ledOn(char color) {
+  if (color == 'g') {
+    PORT_G |= _BV(LED_G); // set high
+  } else if (color == 'b') {
+    PORT_B |= _BV(LED_B);
+  } else if (color == 'r') {
+    PORT_R |= _BV(LED_R);
+  }
+}
+
+inline static void ledOff(char color) {
+  if (color == 'g') {
+    PORT_G &= ~_BV(LED_G); // set low
+  } else if (color == 'b') {
+    PORT_B &= ~_BV(LED_B);
+  } else if (color == 'r') {
+    PORT_R &= ~_BV(LED_R);
+  }
+}
+
+inline static void ledToggle(char color) {
+  if (color == 'g') {
+    PORT_G ^= _BV(LED_G); // invert (exclusive or)
+  } else if (color == 'b') {
+    PORT_B ^= _BV(LED_B);
+  } else if (color == 'r') {
+    PORT_R ^= _BV(LED_R);
+  }
+}
