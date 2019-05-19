3f3947b65ad0bee10a87f068ec28d7bc0ff56315
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2019-05-19 19:58:10 +0200

    can send commands via isr

--- modules/i2c-scanner-isr.c
+++ modules/i2c-scanner-isr.c
@@ -12,6 +12,32 @@
 #include "uart.h"
 #include "TWIlib.h"
 
+
+// oled ssd1306
+#define O_ADDR 0x3c
+#define O_ADDR_W (O_ADDR << 1) & 0xfe
+
+uint8_t send_cmd(unsigned char cmd) {
+  uint8_t data[3];
+  data[0] = O_ADDR_W;
+  data[1] = 0x80; //  command mode
+  data[2] = cmd;
+
+  TWIInfo.errorCode = TWI_NO_RELEVANT_INFO;
+
+  uint8_t timeout = 0;
+  while (TWIInfo.errorCode != TWI_SUCCESS && timeout < 2) {
+    TWITransmitData(data, 3, 0);
+    _delay_ms(1);
+    timeout++;
+  }
+  if (TWIInfo.errorCode == TWI_SUCCESS) {
+    return 0; // success
+  } else {
+    return 1;
+  }
+}
+
 int main(void) {
   DINIT(); // enable debug output
   DL("\n\nHello there");
@@ -21,6 +47,9 @@ int main(void) {
   uint16_t timeout;
   sei();
   TWIInit();
+
+  DF("errors: %u", send_cmd(0xaf)); // oled on
+
   DL("Scanning...");
   for (uint8_t addr = 0; addr<127; addr++) {
     addrw = (addr << 1) & 0xfe;
@@ -28,15 +57,16 @@ int main(void) {
     TWIInfo.errorCode = TWI_NO_RELEVANT_INFO;
 
     timeout = 0;
-    while (TWIInfo.errorCode != 0xFF && timeout < 10) {
+    while (TWIInfo.errorCode != TWI_SUCCESS && timeout < 2) {
       TWITransmitData(txdata, 1, 0);
       _delay_ms(1);
       timeout++;
     }
-    if (TWIInfo.errorCode == 0xFF) {
+    if (TWIInfo.errorCode == TWI_SUCCESS) {
       DF("found device 0x%02X", addr);
     }
   }
   DL("Done.");
+
   return 0;
 }
